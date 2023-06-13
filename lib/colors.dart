6ed3b3311bba02557776308a38a4af760b6f5e27
Shikhar <shikharsharma1408@gamil.com>
2023-06-14 01:19:30 +0530

    contact list  file added

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
 
-const backgroundColor = Color.fromRGBO(182, 215, 234, 1);
-const tabColor = Color.fromRGBO(7, 58, 87, 1);
+const backgroundColor = Color.fromRGBO(255, 255, 255, 1);
+const tabColor = Color.fromRGBO(54, 169, 236, 1);
+const appbarColor = Color.fromRGBO(8, 37, 55, 1);
