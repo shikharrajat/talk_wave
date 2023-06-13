7273a9a1c8cb892878ef55ef198a63cd28fdab4b
Shikhar <shikharsharma1408@gamil.com>
2023-06-14 00:57:26 +0530

    appbar and tabs added

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,3 +1,4 @@
 import 'dart:ui';
 
 const backgroundColor = Color.fromRGBO(182, 215, 234, 1);
+const tabColor = Color.fromRGBO(7, 58, 87, 1);
--- lib/screens/mobilescreenlayout.dart
+++ lib/screens/mobilescreenlayout.dart
@@ -1,12 +1,53 @@
 import 'package:flutter/material.dart';
+import '/colors.dart';
 
 class MobileLayoutScreen extends StatelessWidget {
   const MobileLayoutScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('This is Mobile screen')),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('TalkWave',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold)),
+          centerTitle: false,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.search, color: Colors.grey),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(Icons.more_vert, color: Colors.grey),
+              onPressed: () {},
+            ),
+          ],
+          bottom: const TabBar(
+            indicatorColor: tabColor,
+            indicatorWeight: 4,
+            labelColor: tabColor,
+            unselectedLabelColor: Colors.grey,
+            labelStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+            tabs: [
+              Tab(
+                text: 'CHATS',
+              ),
+              Tab(
+                text: 'STATUS',
+              ),
+              Tab(
+                text: 'CALLS',
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
