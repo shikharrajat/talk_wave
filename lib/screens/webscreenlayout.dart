e4b7a84c3d54947d31b369afab7961123fe47fe0
Shikhar <shikharsharma1408@gamil.com>
2023-06-14 00:01:16 +0530

    responsive layouts added

---
+++ lib/screens/webscreenlayout.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WebLayoutScreen extends StatelessWidget {
+  const WebLayoutScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('This is Web screen')),
+    );
+  }
+}
