a793704504749c05cd08f82f052d6ac0e15cd5fb
Shikhar <shikharsharma1408@gamil.com>
2023-06-14 02:05:58 +0530

    Mobile Contact list updated

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
-const backgroundColor = Color.fromRGBO(255, 255, 255, 1);
+const backgroundColor = Color.fromRGBO(191, 219, 243, 1);
 const tabColor = Color.fromRGBO(54, 169, 236, 1);
 const appbarColor = Color.fromRGBO(8, 37, 55, 1);
+const dividerColor = Color.fromRGBO(205, 227, 249, 1);
