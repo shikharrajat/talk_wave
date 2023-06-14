a3beb681f623cdc95884218bce949e371b14eadd
Shikhar <shikharsharma1408@gamil.com>
2023-06-15 00:06:44 +0530

    colors file updated

--- lib/colors.dart
+++ lib/colors.dart
@@ -7,3 +7,5 @@ const appbarColor = Color.fromRGBO(8, 37, 55, 1);
 const dividerColor = Colors.grey;
 const webAppBarColor = Color.fromRGBO(8, 37, 55, 1);
 const searchBarColor = Color.fromRGBO(8, 37, 55, 1);
+const messageColor = Color.fromRGBO(16, 189, 192, 1);
+const senderMessageColor = Color.fromRGBO(171, 211, 231, 1);
