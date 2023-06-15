f0958fb1a31f2dc6036947f1be504630cae1fd15
Shikhar <shikharsharma1408@gamil.com>
2023-06-16 02:59:08 +0530

    login screen added

---
+++ lib/features/auth/screens/login_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  static const routeName = '/login-screen';
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ENTER YOUR PHONE NUMBER'),
+      ),
+    );
+  }
+}
