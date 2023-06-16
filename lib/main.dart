9ed8a1c9f06dea3d818aeeedce0c429f485fb882
Shikhar <shikharsharma1408@gamil.com>
2023-06-17 04:31:28 +0530

    main.dart updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'screens/mobilescreenlayout.dart';
 import './colors.dart';
 import 'package:talk_wave/firebase_options.dart';
-import 'screens/webscreenlayout.dart';
-import './responsive/responsive_layout.dart';
 import 'features/landing/screens/landing_screen.dart';
 import 'router.dart';
+import 'package:talk_wave/common/widgets/loader.dart';
+import 'package:talk_wave/common/widgets/error.dart';
+import 'features/auth/controller/auth_controller.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,15 +16,15 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const TalkWave());
+  runApp(  const ProviderScope(child:  TalkWave()));
 }
 
-class TalkWave extends StatelessWidget {
+class TalkWave extends ConsumerWidget {
   const TalkWave({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
         title: 'TalkWave',
         theme: ThemeData.dark().copyWith(
@@ -30,6 +32,20 @@ class TalkWave extends StatelessWidget {
           appBarTheme: const AppBarTheme(color: appBarColor),
         ),
         onGenerateRoute: ((settings) => generateRoute(settings)),
-        home: const LandingScreen());
+        home: ref.watch(userDataAuthProvider).when(
+            data: (user) {
+              if (user == null) {
+                return const LandingScreen();
+              }
+              return const MobileLayoutScreen();
+            },
+            error: (err, trace) {
+              return ErrorScreen(
+                error: err.toString(),
+              );
+            },
+            loading: () => const Loader(),
+          ),
+        );
   }
 }
