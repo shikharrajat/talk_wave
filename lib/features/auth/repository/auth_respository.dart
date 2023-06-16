374214ea93d3b465b976223fc24f5bcec5a1c46a
Shikhar <shikharsharma1408@gamil.com>
2023-06-16 13:55:40 +0530

    auth repository file added

---
+++ lib/features/auth/repository/auth_respository.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:talk_wave/common/utils/utils.dart';
+import 'package:talk_wave/features/auth/screens/otp_screen.dart';
+import 'package:talk_wave/features/auth/screens/login_screen.dart';
+import 'package:talk_wave/router.dart';
+
+
+class AuthRepository {
+  final FirebaseAuth auth;
+  final FirebaseFirestore firestore;
+  AuthRepository({
+    required this.auth,
+    required this.firestore,
+  });
+
+  void signInWithPhone(BuildContext context, String phoneNumber) async {
+    try {
+      await auth.verifyPhoneNumber(
+        phoneNumber: phoneNumber,
+        verificationCompleted: (PhoneAuthCredential credential) async {
+          await auth.signInWithCredential(credential);
+        },
+        verificationFailed: (e) {
+          throw Exception(e.message);
+        },
+        codeSent: ((String verificationId, int? resendToken) async {
+          Navigator.pushNamed(
+            context,
+            OTPScreen.routeName,
+            arguments: verificationId,
+          );
+        }),
+        codeAutoRetrievalTimeout: (String verificationId) {},
+      );
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context: context, content: e.message!);
+    }
+  }
+
+  void verifyOTP({
+    required BuildContext context,
+    required String verificationId,
+    required String userOTP,
+  }) async {
+    try {
+      PhoneAuthCredential credential = PhoneAuthProvider.credential(
+        verificationId: verificationId,
+        smsCode: userOTP,
+      );
+      await auth.signInWithCredential(credential);
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context: context, content: e.message!);
+    }
+  }
+}
