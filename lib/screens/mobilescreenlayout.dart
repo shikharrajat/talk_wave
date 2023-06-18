76da3e1aa0f84cd19bdf1b7b28bd518b6fcb6d2b
Shikhar <shikharsharma1408@gamil.com>
2023-06-18 17:26:18 +0530

    online/offline feature added

--- lib/screens/mobilescreenlayout.dart
+++ lib/screens/mobilescreenlayout.dart
@@ -2,11 +2,47 @@ import 'package:flutter/material.dart';
 import 'package:talk_wave/features/select_contacts/screens/select_contact_screen.dart';
 import '/colors.dart';
 import '../features/chat/widgets/contact_list.dart';
+import 'package:talk_wave/features/auth/controller/auth_controller.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MobileLayoutScreen extends StatelessWidget {
+class MobileLayoutScreen extends ConsumerStatefulWidget {
   const MobileLayoutScreen({Key? key}) : super(key: key);
 
   @override
+  ConsumerState<MobileLayoutScreen> createState() => _MobileLayoutScreenState();
+}
+
+class _MobileLayoutScreenState extends ConsumerState<MobileLayoutScreen>
+    with WidgetsBindingObserver {
+
+ @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    switch (state) {
+      case AppLifecycleState.resumed:
+        ref.read(authControllerProvider).setUserState(true);
+        break;
+      case AppLifecycleState.inactive:
+      case AppLifecycleState.detached:
+      case AppLifecycleState.paused:
+        ref.read(authControllerProvider).setUserState(false);
+        break;
+    }
+  }
+ 
+ @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 3,
