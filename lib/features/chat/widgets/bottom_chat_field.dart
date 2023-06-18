3db2c420dfa30dd39ffd7d705fb4365f6b0c0eca
Shikhar <shikharsharma1408@gamil.com>
2023-06-19 05:11:53 +0530

    message reply added in widgets

--- lib/features/chat/widgets/bottom_chat_field.dart
+++ lib/features/chat/widgets/bottom_chat_field.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:talk_wave/colors.dart';
+import 'package:talk_wave/common/utils/colors.dart';
 import 'package:talk_wave/features/chat/controller/chat_controller.dart';
 import 'package:talk_wave/common/enums/message_enum.dart';
 import 'package:talk_wave/common/utils/utils.dart';
@@ -9,10 +9,12 @@ import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter_sound/public/flutter_sound_recorder.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:talk_wave/common/providers/message_relpy_provider.dart';
+import 'package:talk_wave/features/chat/widgets/message_reply_preview.dart';
 
 class BottomChatField extends ConsumerStatefulWidget {
   final String recieverUserId;
-
+ 
   const BottomChatField({
     Key? key,
     required this.recieverUserId,
@@ -25,20 +27,20 @@ class BottomChatField extends ConsumerStatefulWidget {
 class _BottomChatFieldState extends ConsumerState<BottomChatField> {
   bool isShowSendButton = false;
   final TextEditingController _messageController = TextEditingController();
-  bool isShowEmojiContainer = false;
-   FocusNode focusNode = FocusNode();
-    FlutterSoundRecorder? _soundRecorder;
+  FlutterSoundRecorder? _soundRecorder;
   bool isRecorderInit = false;
+  bool isShowEmojiContainer = false;
   bool isRecording = false;
+  FocusNode focusNode = FocusNode();
 
- @override
+  @override
   void initState() {
     super.initState();
     _soundRecorder = FlutterSoundRecorder();
     openAudio();
   }
 
-   void openAudio() async {
+  void openAudio() async {
     final status = await Permission.microphone.request();
     if (status != PermissionStatus.granted) {
       throw RecordingPermissionException('Mic permission not allowed!');
@@ -46,7 +48,6 @@ class _BottomChatFieldState extends ConsumerState<BottomChatField> {
     await _soundRecorder!.openRecorder();
     isRecorderInit = true;
   }
-
 
   void sendTextMessage() async {
     if (isShowSendButton) {
@@ -58,7 +59,7 @@ class _BottomChatFieldState extends ConsumerState<BottomChatField> {
       setState(() {
         _messageController.text = '';
       });
-   } else {
+    } else {
       var tempDir = await getTemporaryDirectory();
       var path = '${tempDir.path}/flutter_sound.aac';
       if (!isRecorderInit) {
@@ -83,9 +84,13 @@ class _BottomChatFieldState extends ConsumerState<BottomChatField> {
     File file,
     MessageEnum messageEnum,
   ) {
-    ref
-        .read(chatControllerProvider)
-        .sendFileMessage(context, file, widget.recieverUserId, messageEnum);
+    ref.read(chatControllerProvider).sendFileMessage(
+          context,
+          file,
+          widget.recieverUserId,
+          messageEnum,
+     
+        );
   }
 
   void selectImage() async {
@@ -102,18 +107,19 @@ class _BottomChatFieldState extends ConsumerState<BottomChatField> {
     }
   }
 
-   void selectGIF() async {
+  void selectGIF() async {
     final gif = await pickGIF(context);
     if (gif != null) {
       ref.read(chatControllerProvider).sendGIFMessage(
             context,
             gif.url,
             widget.recieverUserId,
+   
           );
     }
   }
 
-    void hideEmojiContainer() {
+  void hideEmojiContainer() {
     setState(() {
       isShowEmojiContainer = false;
     });
@@ -142,32 +148,42 @@ class _BottomChatFieldState extends ConsumerState<BottomChatField> {
   void dispose() {
     super.dispose();
     _messageController.dispose();
+    _soundRecorder!.closeRecorder();
+    isRecorderInit = false;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    final messageReply = ref.watch(messageReplyProvider);
+    final isShowMessageReply = messageReply != null;
+    return Column(
       children: [
-        Expanded(
-          child: TextFormField(
-            controller: _messageController,
-            onChanged: (val) {
-              if (val.isNotEmpty) {
-                setState(() {
-                  isShowSendButton = true;
-                });
-              } else {
-                setState(() {
-                  isShowSendButton = false;
-                });
-              }
-            },
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: mobileChatBoxColor,
-              prefixIcon:  Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child:  Row(
+        isShowMessageReply ? const MessageReplyPreview() : const SizedBox(),
+        Row(
+          children: [
+            Expanded(
+              child: TextFormField(
+                focusNode: focusNode,
+                controller: _messageController,
+                onChanged: (val) {
+                  if (val.isNotEmpty) {
+                    setState(() {
+                      isShowSendButton = true;
+                    });
+                  } else {
+                    setState(() {
+                      isShowSendButton = false;
+                    });
+                  }
+                },
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: mobileChatBoxColor,
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: SizedBox(
+                      width: 100,
+                      child: Row(
                         children: [
                           IconButton(
                             onPressed: toggleEmojiKeyboardContainer,
@@ -177,7 +193,7 @@ class _BottomChatFieldState extends ConsumerState<BottomChatField> {
                             ),
                           ),
                           IconButton(
-                            onPressed: () {},
+                            onPressed: selectGIF,
                             icon: const Icon(
                               Icons.gif,
                               color: Colors.grey,
@@ -185,54 +201,65 @@ class _BottomChatFieldState extends ConsumerState<BottomChatField> {
                           ),
                         ],
                       ),
-              ),
-              suffixIcon: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    IconButton(
-                      onPressed: selectImage,
-                      icon: const Icon(Icons.camera_alt),
-                      color: Colors.grey,
                     ),
-                    IconButton(
-                      onPressed: selectVideo,
-                      icon: const Icon(Icons.attach_file),
-                      color: Colors.grey,
+                  ),
+                  suffixIcon: SizedBox(
+                    width: 100,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        IconButton(
+                          onPressed: selectImage,
+                          icon: const Icon(
+                            Icons.camera_alt,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: selectVideo,
+                          icon: const Icon(
+                            Icons.attach_file,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
+                  hintText: 'Type a message!',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    borderSide: const BorderSide(
+                      width: 0,
+                      style: BorderStyle.none,
+                    ),
+                  ),
+                  contentPadding: const EdgeInsets.all(10),
                 ),
               ),
-              hintText: 'Type a message!',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20.0),
-                borderSide: const BorderSide(
-                  width: 0,
-                  style: BorderStyle.none,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                bottom: 8,
+                right: 2,
+                left: 2,
+              ),
+              child: CircleAvatar(
+                backgroundColor: const Color(0xFF128C7E),
+                radius: 25,
+                child: GestureDetector(
+                  child: Icon(
+                    isShowSendButton
+                        ? Icons.send
+                        : isRecording
+                            ? Icons.close
+                            : Icons.mic,
+                    color: Colors.white,
+                  ),
+                  onTap: sendTextMessage,
                 ),
               ),
-              contentPadding: const EdgeInsets.all(10),
             ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(
-            bottom: 8,
-            right: 2,
-            left: 2,
-          ),
-          child: CircleAvatar(
-            backgroundColor: const Color(0xFF128C7E),
-            radius: 25,
-            child: GestureDetector(
-              child: Icon(
-                isShowSendButton ? Icons.send : Icons.mic,
-                color: Colors.white,
-              ),
-              onTap: sendTextMessage,
-            ),
-          ),
+          ],
         ),
         isShowEmojiContainer
             ? SizedBox(
