34f874bddcad5d1906c39d5dac2fb5484634f4c4
Shikhar <shikharsharma1408@gamil.com>
2023-06-19 05:06:12 +0530

    small changes

---
+++ lib/common/utils/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Color.fromARGB(255, 238, 139, 139);
+const tabColor = Color.fromRGBO(54, 169, 236, 1);
+const appbarColor = Color.fromRGBO(8, 37, 55, 1);
+const dividerColor = Colors.grey;
+const webAppBarColor = Color.fromRGBO(8, 37, 55, 1);
+const searchBarColor = Color.fromRGBO(8, 37, 55, 1);
+const messageColor = Color.fromRGBO(16, 189, 192, 1);
+const senderMessageColor = Color.fromRGBO(171, 211, 231, 1);
+const chatBarMessageColor = Color.fromRGBO(36, 56, 70, 1);
+const mobileChatBoxColor = Color.fromRGBO(52, 85, 106, 1);
+const appBarColor = Color.fromRGBO(36, 54, 66, 1);
+const greyColor = Colors.grey;
+const blackColor = Colors.black;
